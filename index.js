2870a6bd6640cd3f3ac3ab327bfb319ef20fe69c
prathmeshkurhade <prathmeshkurhade33@gmail.com>
2025-03-21 16:44:29 +0530

    Updated index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express();
 
+const mongoose = require("mongoose");
+
 const { userRouter }  = require("./routes/user");
 const { courseRouter }  = require("./routes/course");
 const user = require('./routes/user');
@@ -8,8 +10,13 @@ const user = require('./routes/user');
 app.use("/user", userRouter);
 app.use("/course", courseRouter);
 
+async function main() {
+    await mongoose.connect("mongodb://localhost:27017")
+    app.listen(3000);
+    console.log("are u cooked")
+}
 
 
 
 
-app.listen(3000)
+main()
